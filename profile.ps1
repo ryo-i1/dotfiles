c265f284795b439661f548da2d1f36ce799498ad
ryo-i1 <bluestone2853@gmail.com>
2024-07-14 02:32:04 +0900

    oh-my-posh init

--- profile.ps1
+++ profile.ps1
@@ -5,3 +5,5 @@ Set-PSReadLineOption -PredictionSource None
 # scoop-completion
 Import-Module "$($(Get-Item $(Get-Command scoop.ps1).Path).Directory.Parent.FullName)\modules\scoop-completion"
 
+# oh-my-posh
+oh-my-posh init pwsh --config $HOME\scoop\apps\oh-my-posh\current\themes\stelbent-compact.minimal.omp.json | Invoke-Expression
