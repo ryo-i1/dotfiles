e62e4c22667eede67433e2f78d13ab9d46ab1ca9
ryo-i1 <bluestone2853@gmail.com>
2024-07-14 01:18:55 +0900

    add scoop-completion

--- profile.ps1
+++ profile.ps1
@@ -2,3 +2,6 @@
 # 予測を非表示
 Set-PSReadLineOption -PredictionSource None
 
+# scoop-completion
+Import-Module "$($(Get-Item $(Get-Command scoop.ps1).Path).Directory.Parent.FullName)\modules\scoop-completion"
+
